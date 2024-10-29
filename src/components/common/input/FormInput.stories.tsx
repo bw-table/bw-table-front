1effb47b972ff6dede98bb7a8108876d0153bdc7
osw6858 <osw7875@naver.com>
2024-10-29 18:57:21 +0900

    refactor: FormInput 스토리 argTypes을 자동 추론 하도록 명시하지 않고 삭제

--- src/components/common/input/FormInput.stories.tsx
+++ src/components/common/input/FormInput.stories.tsx
@@ -6,44 +6,6 @@ const meta: Meta<typeof FormInput> = {
   title: 'common/FormInput',
   component: FormInput,
   tags: ['autodocs'],
-  argTypes: {
-    type: {
-      control: { type: 'radio' },
-      options: ['text', 'password', 'email', 'number', 'tel'],
-    },
-    variant: {
-      control: { type: 'radio' },
-      options: ['default', 'disabled', 'error'],
-    },
-    disable: {
-      control: { type: 'boolean' },
-    },
-    error: {
-      control: { type: 'object' },
-      defaultValue: {
-        type: 'required',
-        message: '이 필드는 필수입니다',
-      },
-    },
-    required: {
-      control: { type: 'boolean' },
-    },
-    readOnly: {
-      control: { type: 'boolean' },
-    },
-    maxLength: {
-      control: { type: 'number' },
-    },
-    placeholder: {
-      control: { type: 'text' },
-    },
-    label: {
-      control: { type: 'text' },
-    },
-    classNames: {
-      control: { type: 'text' },
-    },
-  },
   decorators: [
     (Story, context) => {
       const methods = useForm();
