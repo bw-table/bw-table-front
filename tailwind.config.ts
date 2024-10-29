25b37dead8712e82d42efc8dac0ea9a1fa3ee2ec
osw6858 <osw7875@naver.com>
2024-10-29 11:53:10 +0900

    style: 공통 색상값 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: {
+          500: '#800020', // 버튼 메인 색상
+        },
+        success: {
+          500: '#2ECC40', // 확정 버튼 색상
+        },
+        danger: {
+          500: '#DC3545', // 취소 버튼 색상
+        },
+        info: {
+          400: '#63B3ED', // 방문 버튼 색상
+        },
+        warning: {
+          500: '#F26B3A', // 노쇼 버튼 색상
+        },
+      },
     },
   },
   daisyui: {
