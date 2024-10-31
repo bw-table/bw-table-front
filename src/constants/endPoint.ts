a3910344bffb842b9c27d9cddd1e3875c3cd8146
osw6858 <osw7875@naver.com>
2024-10-31 21:58:06 +0900

    feat: endPoint 추가

--- src/constants/endPoint.ts
+++ src/constants/endPoint.ts
@@ -1,3 +1,8 @@
 export const END_POINT = {
-  EXAMPLE: '/api/v1/example',
+  EXAMPLE: '/api/example',
+  SIGNUP: '/api/auth/signup',
+  EMAIL_DUPLICATE: '/api/auth/email/duplicate',
+  NICKNAME_DUPLICATE: '/api/auth/nickname/duplicate',
+  TEL_DUPLICATE: '/api/auth/tel/duplicate',
+  BUSINESS_DUPLICATE: '/api/auth/business/duplicate',
 } as const;
