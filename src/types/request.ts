ed12befde364818803f819cd6a7ee5079ec03c4d
Pinn <lsol1130@naver.com>
2024-11-04 19:11:47 +0900

    feat: 식당 상세정보 types 정의

--- src/types/request.ts
+++ src/types/request.ts
@@ -19,3 +19,41 @@ export interface SignInRequestType {
   email: string;
   password: string;
 }
+
+export interface RestaurantDetailRequestType {
+  restaurantId: number,
+  name: string,
+  description: string,
+  address: string,
+  contact: string,
+  closedDay: string,
+  category: string,
+  link: string,
+  rating: number,
+  reviews: [],
+  notices: [
+    { id: number, title: string, content: string },
+  ]
+  images: [],
+  menus: [
+      {
+          id: number,
+          name: string,
+          price: number,
+          description: string,
+          imageUrl: string,
+          restaurantId: number
+      }
+  ],
+  facilities: [],
+  hashtags: [],
+  operatingHours: [
+      {
+          id: number,
+          dayOfWeek: string,
+          openingTime: string,
+          closingTime: string,
+          restaurantId: number
+      }
+]
+}
