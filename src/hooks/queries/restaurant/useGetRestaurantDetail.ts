87c257310b297b77210992487f142d601824b1f7
Pinn <lsol1130@naver.com>
2024-11-05 20:33:39 +0900

    refactor: useEffect훅 제거

--- src/hooks/queries/restaurant/useGetRestaurantDetail.ts
+++ src/hooks/queries/restaurant/useGetRestaurantDetail.ts
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
 import { END_POINT } from '@/constants/endPoint';
 import { axiosDefault } from '@/api/axiosInstance';
 import { RestaurantDetailRequestType } from '@/types';
@@ -22,14 +21,13 @@ export default function useGetRestaurantDetail(restaurantId: number) {
     enabled: !!restaurantId,
   });
 
-  useEffect(() => {
-    if (restaurantDetailIsSuccess) {
-      console.log('데이터 가져오기 성공:', restaurantDetail);
-    }
-    if (restaurantDetailIsError) {
-      console.error('데이터 가져오기 실패:');
-    }
-  }, [restaurantDetailIsSuccess, restaurantDetailIsError, restaurantDetail]);
+  if (restaurantDetailIsSuccess) {
+    console.log('데이터 가져오기 성공:', restaurantDetail);
+  }
+
+  if (restaurantDetailIsError) {
+    console.error('데이터 가져오기 실패:');
+  }
 
   return {
     restaurantDetail,
