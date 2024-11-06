353bf16332152e8e8cbac2871351a9899d528d0e
Pinn <lsol1130@naver.com>
2024-11-07 03:42:44 +0900

    feat: 업종 카테고리 유연한 사용을 위한 상수 정의

---
+++ src/constants/restaurantCategory.ts
@@ -0,0 +1,12 @@
+export const CATEGORY: { [key: string]: string } = {
+  KOREAN: '한식당',
+  JAPANESE: '일식당',
+  CHINESE: '중식당',
+  RESTAURANT: '레스토랑',
+  FINE_DINING: '파인다이닝',
+  OMAKASE: '오마카세',
+  FUSION: '퓨전 음식점',
+  FAST_FOOD: '패스트푸드',
+  BUFFET: '뷔페',
+  ETC: '기타',
+};
