c27dd8652f79b23db9efcaba92d4aef912286ed2
Pinn <lsol1130@naver.com>
2024-11-06 23:14:37 +0900

    feat: 리뷰 리스트 가져오는 쿼리 훅 구현

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,3 +1,4 @@
 export const QUERY_KEYS = {
   EXAMPLE: 'EXAMPLE_KEY',
+  REVIEWS:'reviews'
 } as const;
---
+++ src/hooks/queries/restaurant/useGetReviewList.ts
@@ -0,0 +1,35 @@
+import { axiosAuth } from '@/api/axiosInstance';
+import { END_POINT } from '@/constants/endPoint';
+import { useQuery } from '@tanstack/react-query';
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import { DB } from '@/mocks/db/db';
+
+// 목데이터 사용중
+// const fetchAPI = async (restaurantId: number) => {
+//   const res = await axiosAuth.get(`${END_POINT.RESTAURANTS}/${restaurantId}/reviews`);
+//   return res.data;
+// };
+
+export const useGetReviewList = (restaurantId: number) => {
+  const {
+    data: reviewData,
+    isLoading: isReviewLoading,
+    isSuccess: isReviewLoadingSuccess,
+    isError: isReviewError,
+  } = useQuery({
+    queryKey: [QUERY_KEYS.REVIEWS, restaurantId],
+    queryFn: () => {
+      return DB.reviews.filter(review => review.restaurantId === restaurantId);
+    },
+  });
+
+  if (isReviewLoadingSuccess) {
+    console.log('데이터 가져오기 성공:', reviewData);
+  }
+
+  return {
+    reviewData,
+    isReviewLoading,
+    isReviewError,
+  };
+};
