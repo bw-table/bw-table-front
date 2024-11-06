ebafc514d4abed53a6a61172bb7162d5cbadc02b
Pinn <lsol1130@naver.com>
2024-11-06 23:16:04 +0900

    feat: 네비게이션 바 클릭시 리뷰 리스트 불러오는 걸로 컴포넌트 수정 및 분리

--- src/app/(member)/restaurants/[id]/page.tsx
+++ src/app/(member)/restaurants/[id]/page.tsx
@@ -6,6 +6,7 @@ import CommonButton from '@/components/common/button/CommonButton';
 import CommonNavigationBar from '@/components/common/navigation-bar/CommonNavigationBar';
 import { useRouter } from 'next/navigation';
 import useGetRestaurantDetail from '@/hooks/queries/restaurant/useGetRestaurantDetail';
+import RestaurantReviewTab from '@/components/feature/restaurant/RestaurantReviewTab';
 
 export default function RestaurantDetail() {
   const router = useRouter();
@@ -25,6 +26,7 @@ export default function RestaurantDetail() {
     { key: 'info', label: '매장정보' },
   ];
 
+
   return (
     <div className="p-4 max-w-md mx-auto bg-white relative">
       <div className="mb-4">
@@ -39,7 +41,7 @@ export default function RestaurantDetail() {
         <h1 className="text-2xl font-semibold">{restaurantDetail?.name}</h1>
         <div className="flex items-center text-yellow-500 my-2">
           <AiFillStar />
-          <span className="ml-1">{restaurantDetail?.rating}</span>
+          <span className="ml-1">{restaurantDetail?.averageRating}</span>
         </div>
         <p className="text-gray-500">{restaurantDetail?.address}</p>
         <p className="text-gray-500">{restaurantDetail?.closedDay}</p>
@@ -51,17 +53,20 @@ export default function RestaurantDetail() {
         onSelectTab={setSelectedTab}
         variant="text"
       />
+      {/* 탭에 따라 다른 콘텐츠 표시 */}
+    {/* // {selectedTab === 'home' && (
+    //   <div className="px-4 mb-4">
+    //     <h2 className="text-lg font-semibold">소식</h2>
+    //     {restaurantDetail?.info?.map((notice) => (
+    //       <div key={notice.id} className="p-2 border border-gray-300 rounded mb-2">
+    //         <h3 className="font-semibold text-gray-700">{notice.title}</h3>
+    //         <p className="text-gray-500 text-sm">{notice.content}</p>
+    //       </div>
+    //     ))}
+    //   </div>
+    // )} */}
 
-      <div className="px-4 mb-4">
-        <h2 className="text-lg font-semibold">리뷰</h2>
-        {restaurantDetail?.notices?.map((notice) => (
-          <div key={notice.id} className="p-2 border border-gray-300 rounded mb-2">
-            <h3 className="font-semibold text-gray-700">{notice.title}</h3>
-            <p className="text-gray-500 text-sm">{notice.content}</p>
-          </div>
-        ))}
-      </div>
-
+    {selectedTab === 'menu' && (
       <div className="px-4 mb-4">
         <h2 className="text-lg font-semibold">메뉴</h2>
         {restaurantDetail?.menus?.map((menu) => (
@@ -71,7 +76,15 @@ export default function RestaurantDetail() {
           </div>
         ))}
       </div>
+    )}
 
+    {selectedTab === 'reviews' && (
+      <div className="px-4 mb-4">
+        <h2 className="text-lg font-semibold">리뷰</h2>
+        {/* 리뷰 리스트 컴포넌트를 표시합니다. */}
+        <RestaurantReviewTab restaurantId={restaurantId} />
+      </div>
+    )}
       <div className="fixed inset-x-0 bottom-0 p-4 bg-white border-t border-gray-300 max-w-md mx-auto">
         <CommonButton
           type="button"
---
+++ src/components/feature/restaurant/RestaurantReviewTab.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useGetReviewList } from '@/hooks/queries/restaurant/useGetReviewList';
+import { ReviewFormData } from '@/types';
+import { AiFillStar } from 'react-icons/ai';
+
+interface ReviewListProps {
+  restaurantId: number;
+}
+
+const RestaurantReviewTab: React.FC<ReviewListProps> = ({ restaurantId }) => {
+  const { reviewData, isReviewLoading, isReviewError } = useGetReviewList(restaurantId);
+
+  if (isReviewLoading) {
+    return <p>로딩 중...</p>;
+  }
+
+  if (isReviewError) {
+    return <p>리뷰를 가져오는 데 오류가 발생했습니다.</p>;
+  }
+
+  return (
+    <div className="review-list">
+      {reviewData?.map((review: ReviewFormData) => (
+        <div key={review.id} className="p-4 border-b">
+          <div className="flex gap-2 items-center mb-2">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              alt="User Avatar"
+              className="avatar w-10 h-10 rounded-full"
+            />
+            <p className="font-semibold">{review.userId}</p>
+          </div>                        
+            <div className="my-4">
+              <div className="flex items-center justify-between">
+                <div className='flex'>
+                  <AiFillStar className="text-yellow-500" />
+                  <span className="text-sm font-bold  ml-1">{review.rating}</span>
+                </div>
+                <p className="text-sm text-gray-500">{new Date(review.createdAt).toLocaleDateString()}</p>
+              </div>
+            </div>
+          {review.images && review.images.length > 0 && (
+            <div className="grid grid-cols-2 gap-2">
+              {review.images.map((img, index) => (
+                <img key={index} src={img} alt={`Review Image ${index}`} className="w-full h-auto rounded" />
+              ))}
+            </div>
+          )}
+          <div className="mb-2">
+            <p>{review.content}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default React.memo(RestaurantReviewTab);
