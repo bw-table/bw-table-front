edd3de839f7c90acdeff3d3acec73eed0f9021ad
Pinn <lsol1130@naver.com>
2024-12-12 23:49:45 +0900

    feat: 카테고리 아이콘 이미지 추가

--- src/components/feature/main-page/mainCategoryIcons.tsx
+++ src/components/feature/main-page/mainCategoryIcons.tsx
@@ -1,3 +1,72 @@
+import { BsFillCursorFill } from "react-icons/bs";
+
+export const categories = [
+  { icon:
+      <img
+        src="/assets/stars.png"
+        alt="이달의 맛집"
+        className="w-12 h-12"
+      />,
+      label: "이달의 맛집",
+      badge: "N"
+  },
+  { icon:
+    <img
+      src="/assets/meeting.png"
+      alt="모임 예약"
+      className="w-12 h-12"
+  />,
+    label: "모임 예약"
+  },
+  { icon:
+    <img
+      src="/assets/special.png"
+      alt="스페셜 혜택"
+      className="w-12 h-12"
+  />,
+    label: "스페셜 혜택"
+  },
+  { icon:
+    <img
+      src="/assets/hidden-space.png"
+      alt="히든플레이스"
+      className="w-12 h-12"
+  />,
+    label: "히든플레이스"
+  },
+  { icon:
+      <img
+        src="/assets/omakase.png"
+        alt="오마카세"
+        className="w-12 h-12"
+    />,
+    label: "오마카세"
+  },
+  { icon:     
+      <img
+        src="/assets/chinese.png"
+        alt="중식"
+        className="w-12 h-12"
+      />,
+      label: "중식"
+    },
+  { icon:
+      <img
+      src="/assets/fine-dining.png"
+      alt="파인다이닝"
+      className="w-12 h-12"
+    />,
+    label: "파인다이닝" },
+  { icon:
+    <img
+      src="/assets/pasta.png"
+      alt="파스타"
+      className="w-12 h-12"
+    />,
+    label: "파스타" 
+  },
+];
+
 export const locations = [
   { label: "내주변", icon: <BsFillCursorFill /> },
   { label: "압구정\n청담", icon: "/assets/carouselImage/BWT1.jpg" },
