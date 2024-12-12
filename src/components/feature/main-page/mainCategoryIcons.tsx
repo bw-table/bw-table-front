3c642e40ab5c61955221708b16afab265b717ee7
Pinn <lsol1130@naver.com>
2024-12-12 23:49:21 +0900

    feat: 위치 아이콘 이미지/라벨 추가

---
+++ src/components/feature/main-page/mainCategoryIcons.tsx
@@ -0,0 +1,14 @@
+export const locations = [
+  { label: "내주변", icon: <BsFillCursorFill /> },
+  { label: "압구정\n청담", icon: "/assets/carouselImage/BWT1.jpg" },
+  { label: "부산", icon: "/assets/carouselImage/BWT2.jpg" },
+  { label: "잠실\n송파", icon: "/assets/carouselImage/BWT3.webp" },
+  { label: "이태원\n한남", icon: "/assets/carouselImage/BWT4.jpg" },
+  { label: "성수", icon: "/assets/carouselImage/BWT1.jpg" },
+  { label: "광화문", icon: "/assets/carouselImage/BWT2.jpg" },
+  { label: "용산", icon: "/assets/carouselImage/BWT3.webp" },
+  { label: "신사", icon: "/assets/carouselImage/BWT4.jpg" },
+  { label: "강남", icon: "/assets/carouselImage/BWT1.jpg" },
+  { label: "여의도", icon: "/assets/carouselImage/BWT1.jpg" },
+  { label: "을지로", icon: "/assets/carouselImage/BWT2.jpg" },
+];
