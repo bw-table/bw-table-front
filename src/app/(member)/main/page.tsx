3af9d339842b603a2f667cee4bd0005b90451451
Pinn <lsol1130@naver.com>
2024-12-16 20:41:31 +0900

    feat: 메인페이지 레이아웃 구현

---
+++ src/app/(member)/main/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <>
+      <div className='flex justify-center my-4'>
+        <img src='/Logo.svg' />
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
