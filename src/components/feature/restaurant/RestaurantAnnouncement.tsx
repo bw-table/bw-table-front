045a69453ddfa51febbbe510c7c5c904174a76bb
Pinn <lsol1130@naver.com>
2024-12-17 22:22:25 +0900

    refactor: 명세 상 필요없어진 훅 및 코드 제거

--- src/components/feature/restaurant/RestaurantAnnouncement.tsx
+++ src/components/feature/restaurant/RestaurantAnnouncement.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import { useGetAnnouncement } from '@/hooks/queries/restaurant/useGetAnnouncement';
-import { AnnouncementFormData } from '@/types';
+import { AnnouncementFormData, RestaurantDetailRequestType } from '@/types';
 import Divider from '@/components/common/divider/CommonDivider';
 
 interface AnnouncementListProps {
-  restaurantId: number;
+  announcements: RestaurantDetailRequestType['announcements'];
 }
 
-const RestaurantAnnouncementTab: React.FC<AnnouncementListProps> = ({ restaurantId }) => {
-  const { announcementData, isAnnouncementLoading, isAnnouncementError, } = useGetAnnouncement(restaurantId);
-
-
-  if (isAnnouncementLoading) {
-    return <p>로딩 중...</p>;
-  }
-
-  if (isAnnouncementError) {
-    return <p>공지사항을 가져오는 데 오류가 발생했습니다.</p>;
-  }
-
-  const announcements = Array.isArray(announcementData) ? announcementData : announcementData?.data;
+const RestaurantAnnouncementTab: React.FC<AnnouncementListProps> = ({ announcements }) => {
 
   return (
-    <Divider classNames="notice-list">
-      {announcements && Array.isArray(announcements) && announcements.length > 0 ? (
-        announcements.map((announcement: AnnouncementFormData) => (
-          <div key={announcement.announcementId} className="p-4 border-b">
+    <Divider>
+      {announcements?.map((announcement: AnnouncementFormData) => (
+          <div key={announcement.id} className="p-4 border-b">
             <h2 className="font-semibold mb-2">
               {announcement.isEvent && (
                 <span className="badge bg-yellow-300 text-yellow-800 font-semibold mr-2">
@@ -40,11 +26,9 @@ const RestaurantAnnouncementTab: React.FC<AnnouncementListProps> = ({ restaurant
             <p className="mt-2">{announcement.content}</p>
           </div>
         ))
-      ) : (
-        <p className="text-gray-500">공지사항이 없습니다.</p>
-      )}
+      }
     </Divider>
   );
 };
 
-export default React.memo(RestaurantAnnouncementTab);
+export default RestaurantAnnouncementTab;
