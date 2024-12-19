d4971dff3d351260e522d649e4de2d3deb595816
Pinn <lsol1130@naver.com>
2024-12-19 23:54:31 +0900

    fix: 최초 로그인페이지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import SignInPage from './(auth)/auth/signin/page';
 import Container from '@/components/layout/Container';
+import AuthPage from './(auth)/auth/page';
 
 export default function Home() {
   return (
     <Container variant='mobile'>
-      <SignInPage />
+      <AuthPage />
     </Container>
   );
 }
