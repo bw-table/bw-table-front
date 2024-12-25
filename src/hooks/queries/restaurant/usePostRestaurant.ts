8db311b30c17ea890f27769453e998015c3b5fe9
Pinn <lsol1130@naver.com>
2024-12-25 21:18:50 +0900

    feat: 성공 시 바로 대시보드 화면으로 이동하는 useRouter 추가

--- src/hooks/queries/restaurant/usePostRestaurant.ts
+++ src/hooks/queries/restaurant/usePostRestaurant.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { END_POINT } from '@/constants/endPoint';
 import { axiosAuth } from '@/api/axiosInstance';
+import { useRouter } from 'next/navigation';
 
 const fetchAPI = async (data: FormData) => {
   const response = await axiosAuth.post(END_POINT.NEW_RESTAURANTS, data);
@@ -8,10 +9,12 @@ const fetchAPI = async (data: FormData) => {
 };
 
 export const usePostRestaurant = () => {
+  const router = useRouter();
   const { mutate: submitRestaurant, isPending, isSuccess, isError } = useMutation({
     mutationFn: fetchAPI,
     onSuccess: (data) => {
       console.log('가게 등록 성공:', data);
+      router.push('/management');
     },
     onError: (error) => {
       console.error('가게 등록 실패:', error);
